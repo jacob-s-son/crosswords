af459aad5e27850499e464a357e644153396fa22
Edgars Jekabsons <jekabsons.edgars@gmail.com>
2014-04-12 00:06:51 +0300

    Reverse directions

--- lib/crosswords/word_finder.rb
+++ lib/crosswords/word_finder.rb
@@ -9,15 +9,21 @@ module Crosswords
       case direction
         when 'horizontal_from_the_left'
           all_words_for_letters( @grid.horizontal_letters )
+        when 'horizontal_from_the_right'
+          all_words_for_letters( @grid.horizontal_letters, true )
         when 'vertical_from_the_top'
           all_words_for_letters( @grid.vertical_letters )
+        when 'vertical_from_the_bottom'
+          all_words_for_letters( @grid.vertical_letters, true )
         else
           raise 'Unknown direction'
       end
     end
   private
-    def all_words_for_letters(words_arr)
+    def all_words_for_letters(words_arr, reverse=false)
       words_arr.inject(Set.new) do |words, line_of_letters|
+        line_of_letters = line_of_letters.reverse if reverse
+
         (0..line_of_letters.length - 1).each do |idx|
           if word = find_in_dictionary( line_of_letters[0..-idx-1] )
             words << word
--- spec/crosswords/word_finder_spec.rb
+++ spec/crosswords/word_finder_spec.rb
@@ -24,13 +24,29 @@ describe Crosswords::WordFinder do
           )
         end
       end
+
+      context "right to left" do
+        it "should find words 'AB', 'ABA', 'BA', 'GAB'" do
+          expect( subject.find('horizontal_from_the_right') ).to eq(
+            Set.new ['AB', 'ABA', 'BA', 'GAB']
+          )
+        end
+      end
     end
 
     context "vertical" do
       context "top to bottom" do
-        it "should find words 'AB', 'ABA', 'BA', 'BAG', 'AG', 'CAB'" do
+        it "should find words 'AB', 'BA', 'BAA', 'AA', 'AG'" do
           expect( subject.find('vertical_from_the_top') ).to eq(
             Set.new ['AB', 'BA', 'BAA', 'AA', 'AG']
+          )
+        end
+      end
+
+      context "bottom to top" do
+        it "should find words 'BA', 'AA', 'AB'" do
+          expect( subject.find('vertical_from_the_bottom') ).to eq(
+            Set.new ['BA', 'AA', 'AB']
           )
         end
       end
