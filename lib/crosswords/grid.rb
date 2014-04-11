468bb7d377862cc3a337d98fca2883e8bd1a3c24
Edgars Jekabsons <jekabsons.edgars@gmail.com>
2014-04-11 20:49:10 +0300

    Created generate from matrix method for easy mocking

--- lib/crosswords/grid.rb
+++ lib/crosswords/grid.rb
@@ -4,7 +4,19 @@ module Crosswords
     attr_reader :columns
     attr_reader :rows
 
-    def initialize(n)
+    class << self
+      def generate_from_matrix(matrix)
+        new( nil, matrix )
+      end
+    end
+
+    def initialize(n = nil, matrix = nil)
+      generate_new(n)              if n
+      generate_from_matrix(matrix) if matrix
+    end
+
+  private
+    def generate_new(n)
       @n      = n
       letters = ('A'..'Z').to_a
 
@@ -18,6 +30,20 @@ module Crosswords
           @rows[idx][idx2]  = cell
           @columns[idx2][idx] = cell
         end
+      end
+    end
+
+    def generate_from_matrix(matrix)
+      @n    = matrix.first.size
+      @rows = matrix
+
+      @columns = @rows.each_with_index.inject( [] ) do | cols, (row, idx) |
+        row.each_with_index do |cell, idx2|
+          cols[idx2]      ||= []
+          cols[idx2][idx] ||= cell
+        end
+
+        cols
       end
     end
   end
--- spec/crosswords/grid_spec.rb
+++ spec/crosswords/grid_spec.rb
@@ -6,6 +6,22 @@ describe Crosswords::Grid do
     let(:grid)    { klass.new(n) }
     let(:letters) { ('A'..'Z').to_a }
 
+    let(:matrix) {
+      [
+        ['A', 'B', 'C'],
+        ['B', 'A', 'C'],
+        ['C', 'B', 'A']
+      ]
+    }
+
+    let(:reverse_matrix) {
+      [
+        ['A', 'B', 'C'],
+        ['B', 'A', 'B'],
+        ['C', 'C', 'A']
+      ]
+    }
+
     it "should have N columns" do
       expect( grid.columns.size ).to eq(n)
     end
@@ -24,6 +40,20 @@ describe Crosswords::Grid do
 
     it "should have the same letters in the first row as in the first cell of all columns" do
       expect( grid.rows[0] ).to eq( grid.columns.map { |c| c[0] } )
+    end
+
+    describe ".generate_from_matrix" do
+      it "should generate rows from matrix" do
+        gen_grid = klass.generate_from_matrix( matrix )
+
+        expect( gen_grid.rows ).to eq(matrix)
+      end
+
+      it "should generate columns from matrix" do
+        gen_grid = klass.generate_from_matrix( matrix )
+
+        expect( gen_grid.columns ).to eq(reverse_matrix)
+      end
     end
   end
 end
