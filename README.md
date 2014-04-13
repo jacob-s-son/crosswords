d337a128c17ecf6612ce3e04009921d07230511f
Edgars Jekabsons <jekabsons.edgars@gmail.com>
2014-04-13 20:14:08 +0300

    Cherry-picked changes from master

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 crosswords
 ==========
 
-Make sure to change path in dictionary_spec.rb to Your dictionary file
+Make sure to change path in dictionary_spec.rb and word_finder_spec.rb files to Your dictionary file
+
+
+
+BENCHMARKING dictionary implementations
+
+Master implementation:
+
+Rehearsal --------------------------------------------------
+Each 10th word   1.040000   0.000000   1.040000 (  1.038160)
+Each 5th word    2.080000   0.000000   2.080000 (  2.085326)
+Each 3rd word    3.430000   0.010000   3.440000 (  3.432707)
+Each 2nd word    5.130000   0.010000   5.140000 (  5.142987)
+All words       10.260000   0.010000  10.270000 ( 10.272464)
+---------------------------------------- total: 21.970000sec
+
+                     user     system      total        real
+Each 10th word   1.030000   0.000000   1.030000 (  1.030944)
+Each 5th word    2.060000   0.000000   2.060000 (  2.055734)
+Each 3rd word    3.430000   0.000000   3.430000 (  3.431132)
+Each 2nd word    5.130000   0.000000   5.130000 (  5.134714)
+All words       10.310000   0.010000  10.320000 ( 10.372485)
+
+
+
+Alternative tree like implementation
+
--- main.rb
+++ main.rb
@@ -2,8 +2,22 @@ require "./lib/crosswords"
 
 path = "./dict.txt"
 
-each_10th_word = IO.readlines(path).each_with_index.inject([]) do |arr, (line, idx)|
-  idx % 10 == 0 ? arr << line.match(/^([A-Z]+)\s/)[1].to_s : arr
+all_words = IO.readlines(path).map {|line| line.match(/^([A-Z]+)\s/)[1].to_s }
+
+each_2nd_word = all_words.each_with_index.inject([]) do |arr, (line, idx)|
+  idx % 2 == 0 ? arr << line : arr
+end
+
+each_3rd_word = all_words.each_with_index.inject([]) do |arr, (line, idx)|
+  idx % 3 == 0 ? arr << line : arr
+end
+
+each_5th_word = all_words.each_with_index.inject([]) do |arr, (line, idx)|
+  idx % 5 == 0 ? arr << line : arr
+end
+
+each_10th_word = all_words.each_with_index.inject([]) do |arr, (line, idx)|
+  idx % 10 == 0 ? arr << line : arr
 end
 
 dictionary = Crosswords::Dictionary.new(path)
@@ -11,8 +25,15 @@ dictionary = Crosswords::Dictionary.new(path)
 
 require 'benchmark'
 
-puts "Time for loading #{each_10th_word.size} words"
+puts "Time for finding words in dictionary"
+
 Benchmark.bmbm do |bench|
-  bench.report { each_10th_word.each {|w| dictionary.find( w ) } }
+  bench.report("Each 10th word") { each_10th_word.each {|w| dictionary.find( w ) } }
+  bench.report("Each 5th word")  { each_5th_word.each  {|w| dictionary.find( w ) } }
+  bench.report("Each 3rd word")  { each_3rd_word.each  {|w| dictionary.find( w ) } }
+  bench.report("Each 2nd word")  { each_2nd_word.each  {|w| dictionary.find( w ) } }
+  bench.report("All words")      { all_words.each      {|w| dictionary.find( w ) } }
 end
+
+
 
